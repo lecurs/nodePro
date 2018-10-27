ee07d7d8ea03b5f4fc529b76cdfe623744461ed1
Brynhild <402904972@qq.com>
2018-10-27 22:02:18 +0800

    增加查询路由

--- routes/xiajing.js
+++ routes/xiajing.js
@@ -3,10 +3,17 @@ var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
-router.get('/', async function (req, res) {//增加
-    // let params = req.body; 
-    // await client.post("/students/", params);
-    res.send("suc");
+router.get('/users', async function (req, res) {
+    let params = req.query;
+    let privilege = params.privilege;   
+    let page = params.page || 1;
+    let rows = params.rows || 10;
+    let data = await client.get("/users", {
+        page,
+        rows,
+        privilege
+    });
+    res.send(data);
 
 });
 
