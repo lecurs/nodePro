f26b2f02e0dc6e0020dcd2adc7a7a75dc09e7785
wang qin bing <821462045@qq.com>
2018-10-29 10:39:00 +0800

    获取订单列表

--- routes/wangqinbing.js
+++ routes/wangqinbing.js
@@ -3,5 +3,9 @@ var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
+router.get('/',async function(req,res) {
+    let data =await client.get('/orders',{submitType: 'findJoin', ref: 'stores'})
+    res.send(data)
+})
 
 module.exports = router;
