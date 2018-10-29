7b5f1c073fe3cbd5c92aa1438019b4425b0b3c61
zy <18408256928@163.com>
2018-10-29 10:05:07 +0800

    新增

--- routes/zhangyuan.js
+++ routes/zhangyuan.js
@@ -3,5 +3,15 @@ var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
+router.get('/', async function (req, res) {//增加
+    // let params = req.body; 
+    // await client.post("/students/", params);
+    res.send("suc");
+
+});
+
+
+
+
 
 module.exports = router;
