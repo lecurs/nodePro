04263e19b894d7ef6f2b70ac5b739fbb9b389026
XiongPlus <251891863@qq.com>
2018-10-29 10:51:45 +0800

    编辑接å店铺列表接口

--- routes/xiongwei.js
+++ routes/xiongwei.js
@@ -3,5 +3,10 @@ var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
-
+// 渲染店铺列表
+router.get('/:id', async function (req, res) {
+    let {owner}=req.quary;
+    let data = await client.get('/stores' , { owner,submitType: 'findJoin', ref: ['orders','users']});
+    res.send(data);
+});
 module.exports = router;
