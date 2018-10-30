100fe94cc2d7566bb31dc8b6014ce77e0195e43f
e <zqlong102@126.com>
2018-10-30 20:21:23 +0800

    阶段

--- routes/zhaoqinglong.js
+++ routes/zhaoqinglong.js
@@ -7,22 +7,35 @@ const multiparty = require('multiparty');
 
 //表格渲染
 router.get('/all', async function (req, res) {
-    console.log(req.query.name,req.query.value,"=======================")
+    // console.log(req.query.name,req.query.value,"=======================")
     let { name, value, page, rows } = req.query;
     let searchObj = {};
     if (name) {
         searchObj[name] = value;
     }
     let data = await client.get('/stores', {page,rows,submitType:'findJoin',ref:'classes',...searchObj});
-    res.send(data);
+    let dataAry = [];
+    for(let item of data){
+        if(item.passed=="1"){
+            dataAry.push(item);
+        }
+    }
+    res.send(dataAry);
 });
 
 //增加
 router.post('/addStore', async function (req, res) {
     let body = req.body;
-    await client.post('/stores', body);
-    res.send('suc');
+    let data=  await client.post('/stores', body);
+    res.send(data);
 });
+//增加application
+router.post('/saveApplication', async function (req, res) {
+    let body = req.body;
+    await client.post('/applications', body);
+    res.send("suc");
+});
+
 
 // 删除商店信息
 router.delete('/:id', async function (req, res) {
@@ -48,8 +61,11 @@ router.put('/:id', async function (req, res) {
         $id:body.classes
     };
     let id=req.params.id;
-    await client.put('/stores/'+id,{name:body.name,licenseCode:body.licenseCode,address:body.address,
-        location:body.location,legalPerson:body.legalPerson,phone:body.phone,special:body.special,vip:body.vip,rate:body.rate});
+    if(body.passed=="1"){
+        await client.put('/stores/'+id,{name:body.name,licenseCode:body.licenseCode,address:body.address,
+            location:body.location,legalPerson:body.legalPerson,phone:body.phone,special:body.special,
+            vip:body.vip,licenseImg:body.licenseImg,shopImg:body.shopImg,rate:body.rate});
+    };
     res.send('suc')
 });
 
@@ -65,7 +81,15 @@ router.post('/upload',function(req,res){
       }
     })
   })
-  
 
+
+//商店关联添加到owners集合
+router.put('/owner/:id', async function (req, res) {
+    let id = req.params.id;
+    let data = await client.get("/owners/"+id);
+    data.stores.push(req.body.newStore);
+    await client.put("/owners/"+id,{stores:data.stores}) 
+    res.send('suc')
+});
 
 module.exports = router;
