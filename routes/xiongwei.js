3cf3885bb049e61c3128229efb6ee17987788214
XiongPlus <251891863@qq.com>
2018-10-30 11:54:27 +0800

    â查找特定商店router

--- routes/xiongwei.js
+++ routes/xiongwei.js
@@ -2,11 +2,25 @@ var express = require('express');
 var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
+const _ =require("lodash");
 
 // 渲染店铺列表
+router.get('/', async function (req, res) {
+    let { ownerId } = req.query;
+    // let data = await client.get('/stores', { ownerId, submitType: 'findJoin', ref: ['orders', 'users'] });
+    let data = await client.get('/stores');
+    data=_.filter(data,function(item){
+        return item.owner==ownerId
+    })
+    res.send(data);
+});
+
 router.get('/:id', async function (req, res) {
-    let {owner}=req.quary;
-    let data = await client.get('/stores' , { owner,submitType: 'findJoin', ref: ['orders','users']});
+    let id = req.params.id;
+    let data = await client.get('/stores');
+    data=_.filter(data,function(item){
+        return item.owner==id
+    })
     res.send(data);
 });
 module.exports = router;
