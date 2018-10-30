e1f18301f4df13f564a7240be2ce661b0955f3e1
XiongPlus <251891863@qq.com>
2018-10-30 18:28:11 +0800

    增加商品第一次修改

--- routes/xiongwei.js
+++ routes/xiongwei.js
@@ -15,7 +15,7 @@ router.get('/', async function (req, res) {
     res.send(data);
 });
 
-// 获取制定门店
+// 获取指定门店
 router.get('/:id', async function (req, res) {
     let id = req.params.id;
     let data = await client.get('/stores');
@@ -26,9 +26,9 @@ router.get('/:id', async function (req, res) {
 });
 
 // 获取门店商品
-router.get('/myGoods/:id', async function (req, res) {
+router.get('/goodsOfStore/:id', async function (req, res) {
     let id = req.params.id;
-    let data = await client.get('/goods',{submitType: 'findJoin', ref: 'stores'});
+    let data = await client.get('/goods',{submitType: 'findJoin', ref: ['stores','owners']});
     data=_.filter(data,function(item){
         return item.stores._id==id
     })
@@ -36,12 +36,39 @@ router.get('/myGoods/:id', async function (req, res) {
 });
 
 // 获取门店服务
-router.get('/myServices/:id', async function (req, res) {
+router.get('/servicesOfStore/:id', async function (req, res) {
     let id = req.params.id;
     let data = await client.get('/services',{submitType: 'findJoin', ref: 'stores'});
     data=_.filter(data,function(item){
         return item.stores._id==id
     })
     res.send(data);
+});
+
+// 获取店主商品
+router.get('/getMyGoods/:id', async function (req, res) {
+    let id = req.params.id;
+    let data = await client.get('/goods',{submitType: 'findJoin', ref: ['stores','owners']});
+    data=_.filter(data,function(item){
+        return item.owners._id==id
+    })
+    res.send(data);
+});
+
+// 添加门店商品
+router.get('/goodsOfStores/:id', async function (req, res) {
+    let id = req.params.id;
+    let {storeId}=req.query;
+    console.log(storeId);
+    let data = await client.get('/goods/'+id);
+    let ary=_.filter(data.stores,function(item){
+        return item.$id==storeId;
+    });
+    console.log(ary)
+    if(ary.length==0){
+        data.stores.push({$ref:'stores',$id:`Objected("${storeId}")`})
+        await client.put('/goods/'+id,{stores:data.stores});
+    }
+    res.send('suc');
 });
 module.exports = router;
