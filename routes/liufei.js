8fbe3569570c196f6ba0efd459efe54378474f49
liufei <605985370@qq.com>
2018-10-30 20:23:29 +0800

    保存

--- routes/liufei.js
+++ routes/liufei.js
@@ -6,32 +6,39 @@ client.url("localhost:8080");
 //修改
 router.get('/:id', async function (req, res) {
     let id = req.params.id;
-    let data=await client.get('/students/'+id);
+    let data=await client.get('/owners/'+id, {submitType:'findJoin',ref:'users'});
     res.send(data);
 });
 
 //确认修改
 router.put('/:id', async function (req, res) {
     let body = req.body;
-    body.classes={
-        $ref:'classes',
-        $id:body.classes
-    };
+    // body.users={
+    //     $ref:'users',
+    //     $id:body.users
+    // };
     let id=req.params.id;
-    await client.put('/owners/'+id,{name:body.name,age:body.age,gender:body.gender,imgPath:body.imgPath});
+    await client.put('/users/'+id,{user:body.user,phone:body.phone,pwd:body.pwd,role:body.role,privilege:body.privilege});
     // let data = await client.get('/students/'+id);
-    // console.log(121212,data)
     res.send('suc')
 });
 
 //渲染
 router.get('/', async function (req, res) {
-    let { name, value, page, rows } = req.query;
-    let searchObj = {};
-    if (name) {
-        searchObj[name] = value;
-    }
-    let data = await client.get('/owners', {page,rows,submitType:'findJoin',ref:'classes',...searchObj});
+    
+    let data = await client.get('/owners', {submitType:'findJoin',ref:'users'});
+    // console.log(data);
     res.send(data);
+});
+
+// 新增
+router.post('/', async function (req, res) {
+    let body = req.body;
+    // body.classes={
+    //     $id:body.classes,
+    //     $ref:'classes'
+    // };
+    await client.post('/applications', body);
+    res.send('suc');
 });
 module.exports = router;
