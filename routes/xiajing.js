13ee78581b6d9182245cbc2a166aefe19556c60a
Brynhild <402904972@qq.com>
2018-10-30 17:33:00 +0800

    店铺信息审核路由

--- routes/xiajing.js
+++ routes/xiajing.js
@@ -51,24 +51,21 @@ router.get('/usersAccount', async function (req, res) {
     let page = params.page || 1;
     let rows = params.rows || 10;
     let user = params.user;
-    let passed=params.passed;    
+    let passed = params.passed;
     let data = await client.get("/users", {
         page,
         rows,
         privilege,
         user,
         passed
-    });   
+    });
     res.send(data);
 
 
 });
 
-router.put('/updateUser/:id', async function (req, res) { //修改数据
+router.put('/updateUser/:id', async function (req, res) { //修改用户数据
     let id = req.params.id;
-    console.log('====================================');
-    console.log(req.body.privilege);
-    console.log('====================================');
     await client.put("/users/" + id, {
         "phone": req.body.phone,
         "user": req.body.user,
@@ -82,17 +79,115 @@ router.put('/updateUser/:id', async function (req, res) { //修改数据
 
 
 // =============用户账户审核==============
-router.get('/getApplications', async function (req, res) { //获取管理员账户
+router.get('/getApplications', async function (req, res) { //获取审核
     let params = req.query;
-    let source=params.source;
+    let source = params.source;
+    let handle = params.handle;
     let page = params.page || 1;
     let rows = params.rows || 10;
     let data = await client.get("/applications", {
         page,
         rows,
-        source
+        source,
+        handle
     });
     res.send(data);
 });
+router.put('/updateApplications/:id', async function (req, res) { //更改审批数据状态
+    let id = req.params.id;
+    let passed = req.body.passed;
+    let handle = req.body.handle;
+    let data = await client.get("/applications/" + id);
+    data.passed = passed;
+    data.handle = handle;
+    await client.put("/applications/" + id, {
+        "source": data.source,
+        "tiem": data.tiem,
+        "content": data.content,
+        "passed": data.passed,
+        "handle": data.handle
+    });
+    res.send("suc");
+});
+
+router.put('/updateUserPassed/:id', async function (req, res) { //更改用户账户状态
+    let id = req.params.id;
+    let passed = req.body.passed;
+    let ownnerid = req.body.ownnerid;
+    let newMsg = req.body.newMsg;
+    let data = await client.get("/users/" + id);
+    if (newMsg) {
+        data.phone = newMsg.phone;
+        data.pwd = newMsg.pwd;
+    }
+    if (ownnerid) {
+        data.ownnerid = ownnerid;
+    }
+    data.passed = passed;
+    await client.put("/users/" + id, {
+        "user": data.user,
+        "phone": data.phone,
+        "pwd": data.pwd,
+        "passed": data.passed,
+        "privilege": data.privilege,
+        "ownerId": data.ownnerid
+    });
+    res.send(data);
+});
+
+//修改已通过用户状态
+
+router.post('/addOwners', async function (req, res) { //给已通过的用户创建owner
+    let params = req.body;
+    let data = await client.post("/owners/", params);
+    res.send(data);
+
+});
+
+
+// ========修改已审核店铺的状态=======
+router.put('/updateStorePassed/:id', async function (req, res) { //更改店铺状态
+    let id = req.params.id;
+    let passed = req.body.passed;
+    let newMsg = req.body.newMsg;
+    console.log('====================================');
+    console.log(newMsg);
+    console.log('====================================');
+    let data = await client.get("/stores/" + id);
+    if (newMsg) {
+        data.name=newMsg.name
+        data.licenseCode=newMsg.licenseCode
+        data.licenseImg=newMsg.licenseImg 
+        data.adress=newMsg.adress 
+        data.location=newMsg.location 
+        data.legalPerson =newMsg.legalPerson
+        data.phone=newMsg.phone
+        data.shopImg =newMsg.shopImg
+        data.special=newMsg.special
+        data.vip=newMsg.vip
+        data.rate=newMsg.rate
+    }
+    console.log('================更改后店铺的信息====================');
+    console.log(data);
+    console.log('====================================');
+   let result= await client.put("/stores/" + id, {
+        "name":data.name,
+        "licenseCod":data.licenseCod,
+        "licenseImg":data.licenseImg,
+        "adress":data.adress,
+        "location":data.location,
+        "legalPerson":data.legalPerson,
+        "phone":data.phone,
+        "shopImg":data.shopImg,
+        "special":data.special,
+        "vip":data.vip,
+        "rate":data.rate
+   });
+    console.log('====================================');
+    console.log(result);
+    console.log('====================================');
+    res.send(result);
+});
+
 
 module.exports = router;
