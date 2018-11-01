b4c4fe8fe76cdefb4283a48c72b853e22f528f56
e <zqlong102@126.com>
2018-11-01 10:39:01 +0800

    我ç门门店修改

--- routes/zhaoqinglong.js
+++ routes/zhaoqinglong.js
@@ -5,28 +5,37 @@ client.url("localhost:8080");
 const path = require('path');
 const multiparty = require('multiparty');
 
-//表格渲染
-router.get('/all', async function (req, res) {
-    // console.log(req.query.name,req.query.value,"=======================")
-    let { name, value, page, rows } = req.query;
-    let searchObj = {};
-    if (name) {
-        searchObj[name] = value;
-    }
-    let data = await client.get('/stores', {page,rows,submitType:'findJoin',ref:'classes',...searchObj});
+//查询
+router.get('/all/:id', async function (req, res) {
+    console.log(1213131)
+    let id = req.params.id;
+    let data = await client.get('/stores');
+    data = data.filter(function (item, index) { return item.owner == id });
+    data = data.filter(function (item, index) { return item.passed == "1" });
+    let { name, value} = req.query;
     let dataAry = [];
-    for(let item of data){
-        if(item.passed=="1"){
+    for (let item of data) {
+        if (item.name == value) {
             dataAry.push(item);
         }
     }
+    console.log(data,5757565678)
     res.send(dataAry);
 });
+
+//表格渲染
+router.get('/ownerstore/:id', async function (req, res) {
+    let id = req.params.id;
+    let data = await client.get('/stores');
+    data = data.filter(function (item, index) { return item.owner == id });
+    data = data.filter(function (item, index) { return item.passed == "1" });
+    res.send(data)
+})
 
 //增加
 router.post('/addStore', async function (req, res) {
     let body = req.body;
-    let data=  await client.post('/stores', body);
+    let data = await client.post('/stores', body);
     res.send(data);
 });
 //增加application
@@ -40,8 +49,8 @@ router.post('/saveApplication', async function (req, res) {
 // 删除商店信息
 router.delete('/:id', async function (req, res) {
     let id = req.params.id;
-    console.log(id,12345);
-    await client.delete('/stores/'+id);
+    console.log(id, 12345);
+    await client.delete('/stores/' + id);
     res.send('suc');
 });
 
@@ -49,46 +58,48 @@ router.delete('/:id', async function (req, res) {
 //获取单个id
 router.get('/:id', async function (req, res) {
     let id = req.params.id;
-    let data=await client.get('/stores/'+id);
+    let data = await client.get('/stores/' + id);
     res.send(data);
 });
 
 //修改
 router.put('/:id', async function (req, res) {
     let body = req.body;
-    body.classes={
-        $ref:'classes',
-        $id:body.classes
+    body.classes = {
+        $ref: 'classes',
+        $id: body.classes
     };
-    let id=req.params.id;
-    if(body.passed=="1"){
-        await client.put('/stores/'+id,{name:body.name,licenseCode:body.licenseCode,address:body.address,
-            location:body.location,legalPerson:body.legalPerson,phone:body.phone,special:body.special,
-            vip:body.vip,licenseImg:body.licenseImg,shopImg:body.shopImg,rate:body.rate});
+    let id = req.params.id;
+    if (body.passed == "1") {
+        await client.put('/stores/' + id, {
+            name: body.name, licenseCode: body.licenseCode, address: body.address,
+            location: body.location, legalPerson: body.legalPerson, phone: body.phone, special: body.special,
+            vip: body.vip, licenseImg: body.licenseImg, shopImg: body.shopImg, rate: body.rate
+        });
     };
     res.send('suc')
 });
 
 //上传图片
-router.post('/upload',function(req,res){
+router.post('/upload', function (req, res) {
     // console.log(123456);
-    let form = new multiparty.Form({uploadDir:"./public/upload"});
-    form.parse(req,function(err,fields,files){
-      if(err){
-        res.send(err);
-      }else{
-        res.send(path.basename(files.uploadHeader[0].path));
-      }
+    let form = new multiparty.Form({ uploadDir: "./public/upload" });
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(path.basename(files.uploadHeader[0].path));
+        }
     })
-  })
+})
 
 
 //商店关联添加到owners集合
 router.put('/owner/:id', async function (req, res) {
     let id = req.params.id;
-    let data = await client.get("/owners/"+id);
+    let data = await client.get("/owners/" + id);
     data.stores.push(req.body.newStore);
-    await client.put("/owners/"+id,{stores:data.stores}) 
+    await client.put("/owners/" + id, { stores: data.stores })
     res.send('suc')
 });
 
